c0d72d82e8de2943a23e1ad062cb1759f922f4fd
amritanagekar <31997503+amritanagekar@users.noreply.github.com>
2024-01-30 19:04:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -16,33 +16,14 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    emailid = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
-    todos = db.relationship("Todo", backref="user")
+load_dotenv()
 
-    def __repr__(self) -> str:
-        return f"{self.id} - {self.username}"
+from models import db, User,Todo
 
-    def __init__(self, emailid, password, username):
-        self.username = username
-        self.emailid = emailid
-        self.password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-
-    def check_password(self, password):
-        stored_password = self.password.encode("utf-8")
-        input_password = password.encode("utf-8")
-        return bcrypt.checkpw(input_password, stored_password)
-
-
-class Todo(db.Model):
-    serial_no = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=True)
-    description = db.Column(db.String(500), nullable=True)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+db.init_app(app)
 
 
 # db.create_all()
