f7ae42695919b8b5aa7013f3a90e00b8310ee8b3
amritanagekar <31997503+amritanagekar@users.noreply.github.com>
2024-01-30 12:55:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from datetime import datetime
 from flask_mysqldb import MySQL
 import bcrypt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 app = Flask(__name__)
-app.secret_key = "many random bytes"
+app.secret_key = os.getenv("SECRET_KEY")
 
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "Mypassword@123"
-app.config["MYSQL_DB"] = "crud"
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 mysql = MySQL(app)
 
